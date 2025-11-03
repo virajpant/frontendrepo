9e0949c4667d656b9924fe629427434ec26738a4
Yogesh Pal <94790300+Pal-Yogesh@users.noreply.github.com>
2025-11-03 16:58:15 +0530

    Fix indentation in AllUsers component

--- components/AllUsers.jsx
+++ components/AllUsers.jsx
@@ -72,7 +72,7 @@ const AllUsers = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 p-6 ml-[18%]">
+    <div className="min-h-screen bg-gray-50 p-6 md:ml-[18%]">
       <div className="max-w-7xl mx-auto">
         {/* Header */}
         <div className="mb-8">
